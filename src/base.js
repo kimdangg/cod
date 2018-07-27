14d9ce4ed32a9fb4ff607a0c300a42f952de8de5
Kim Dang <kimdangg@gmail.com>
2018-07-26 21:53:07 -0700

    lesson 18: persist data w/firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBErcGqEg2PmOVrLwx_DcgUno21mGv1ht0",
+  authDomain: "catch-of-the-day-kim-dang.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-kim-dang.firebaseio.com"
+});
+
+export default base;
